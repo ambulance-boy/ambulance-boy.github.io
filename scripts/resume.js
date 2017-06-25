7eaec54b59d2a04b5689fe4e31c50a84474fcb9b
Daniel <dds2130@rit.edu>
2017-06-24 22:33:17 -0400

    hidden scroll bar, floating header

--- scripts/resume.js
+++ scripts/resume.js
@@ -5,30 +5,38 @@
   // + "/od6/public/basic?alt=json"; // Alternate data format
 
   $.getJSON(url, function(data) {
-    var resume = document.getElementById("resume");
+    var resume = document.createElement("div");
+    resume.id = "resume";
 
     $(data.feed.entry).each(function(){
       // Column names are name, age, etc.
       // $('.results').prepend('<h2>'+this.gsx$name.$t+'</h2><p>'+this.gsx$age.$t+'</p>');
-       var title = document.createElement("p");
-       title.className += "MsoBodyText";
+      var title = document.createElement("p");
+      title.className += "MsoBodyText";
 
-       var titleSpan = document.createElement("span");
-       titleSpan.appendChild(document.createTextNode(this.gsx$title.$t));
-       title.appendChild(titleSpan);
+      var titleSpan = document.createElement("span");
+      titleSpan.appendChild(document.createTextNode(this.gsx$title.$t));
+      title.appendChild(titleSpan);
 
-       var artist = document.createElement("p");
-       artist.className += "MsoBodyText";
-       var artistSpan = document.createElement("span");
+      var artist = document.createElement("p");
+      artist.className += "MsoBodyText";
+      var artistSpan = document.createElement("span");
 
-       artistSpan.style.fontSize = "10pt";
-       artistSpan.appendChild(document.createTextNode(this.gsx$artist.$t));
-       artist.appendChild(artistSpan);
+      artistSpan.style.fontSize = "10pt";
+      artistSpan.appendChild(document.createTextNode(this.gsx$artist.$t));
+      artist.appendChild(artistSpan);
 
-       resume.appendChild(document.createElement("br"));
-       resume.appendChild(title);
-       resume.appendChild(artist);
-   });
+      resume.appendChild(title);
+      resume.appendChild(artist);
+      resume.appendChild(document.createElement("br"));
+    });
 
+    $( document ).ready(function() {
+      var parent = document.getElementById('container1');
+      var child = document.getElementById('container2');
+      child.appendChild(resume);
+
+      child.style.paddingRight = child.offsetWidth - child.clientWidth + "px"; // Adjust for scrollbar
+    });
   });
 })();
