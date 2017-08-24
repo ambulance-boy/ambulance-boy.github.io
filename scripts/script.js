272f66b1f1b9aff04325717d2266f81aa82c0c1d
Daniel <dds2130@rit.edu>
2017-08-23 22:15:22 -0400

    on larger screens background is centered

--- scripts/script.js
+++ scripts/script.js
@@ -4,10 +4,49 @@ window.addEventListener("resize", resize);
 function resize() {
     header();
     if ($(window).height() < $('#background').height()) {
-        $(window).bind('mousewheel', static_background);
+        remove_static_background();
+        $(window).bind('mousewheel', stationary_background);
     }
     else {
         $(window).unbind('mousewheel');
+        add_static_background();
+    }
+}
+
+
+
+// Creates sticky header and offsets content below it
+function header() {
+  var header_wrap = document.getElementById('header-wrap');
+  var description = document.getElementById('description');
+  header_wrap.style.position = "fixed";
+  description.style.paddingTop = header_wrap.offsetHeight;
+}
+
+function add_static_background() {
+    var background = $('#background');
+    var header_wrap = $('#header-wrap');
+    background.addClass("stationary");
+    background.css("top", function() {
+        return ( $(window).height() - header_wrap.offsetHeight - background.offsetHeight/2);
+    });
+}
+
+function remove_static_background() {
+    var background = $('#background');
+    background.removeClass("stationary");
+}
+
+function stationary_background(event, delta) {
+    var background = $('#background');
+    if ($('#resume').offset().top < 0) {
+        if (!background.hasClass("stationary")) {
+            background.addClass("stationary");
+            background.css("top", 0);
+        }
+    }
+    else if (background.hasClass("stationary")) {
+        background.removeClass("stationary");
     }
 }
 
@@ -20,24 +59,3 @@ function resize() {
 //         default: return;
 //     }
 // });
-
-// Creates sticky header and offsets content below it
-function header() {
-  var header_wrap = document.getElementById('header-wrap');
-  var description = document.getElementById('description');
-  header_wrap.style.position = "fixed";
-  description.style.paddingTop = header_wrap.offsetHeight;
-}
-
-function static_background(event, delta) {
-    var background = $('#background');
-    if ($('#resume').offset().top < 0) {
-        if (!background.hasClass("stationary")) {
-            background.addClass("stationary");
-        }
-    }
-    else if (background.hasClass("stationary")) {
-        background.removeClass("stationary");
-    }
-}
-
